c15919081107166e9912955ce0567cff69412b1b
marcelomoreiravelloso <marcelo@chromosplanet.com.br>
2021-11-13 11:47:11 -0300

    third commit

---
+++ contact-management/contact-management.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+* Plugin Name: Contact Management
+* Plugin URI: https://alfasoft.pt
+* Description: A Custom Plugin to manage contacts 
+* Version: 1.0.0
+* Author: Marcelo Moreira Velloso
+* Author URI: https://alfasoft.pt
+* License: GPL2
+*/
